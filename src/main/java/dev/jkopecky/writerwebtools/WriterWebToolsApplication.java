e90933694b3a05944aa88f2e7a83d5d72c118d59
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-12-27 17:47:27 -0600

    Initial commit

---
+++ src/main/java/dev/jkopecky/writerwebtools/WriterWebToolsApplication.java
@@ -0,0 +1,13 @@
+package dev.jkopecky.writerwebtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterWebToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WriterWebToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jkopecky/writerwebtools/WriterWebToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jkopecky.writerwebtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriterWebToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
